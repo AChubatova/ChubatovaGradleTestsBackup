316edfa0c9d411eef1c3a78d0586a2b529ce1781
AChubatova <n.chubatova@gmail.com>
2021-07-06 18:11:51 +0200

    update junit

---
+++ src/test/java/my/tiger.java
@@ -0,0 +1,43 @@
+package my;
+
+//import junit.framework.TestCase;
+//import org.junit.Assert;
+//import org.junit.Test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+
+public class tiger {
+
+    @Test
+    public void foo1() {
+        if (System.currentTimeMillis()%3==1){
+        }
+        else {
+            assertEquals(2, 1);
+        };
+
+    };
+    @Test
+    public void foo2() {
+        if (System.currentTimeMillis()%3==1){
+        }
+        else {
+            assertEquals(2, 1);
+        };
+
+    };
+    @Test
+    public void foo3() {
+        if (System.currentTimeMillis()%3==1){
+        }
+        else {
+            assertEquals(2, 1);
+        };
+
+    };
+
+}
